1d496bb250af7748624dca5e5fe22c16c15f5ee0
Marcos <marcos.chiu@gmail.com>
2026-03-12 18:10:49 -0300

    basic arg treat

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,6 +1,16 @@
 module MyLib (someFunc) where
 
 someFunc :: String -> IO Int
-someFunc arg = do
-  putStrLn arg
-  return 0
+someFunc arg = parseArgs arg
+  where
+    parseArgs a
+      | a `elem` ["--help","-h"] = do
+          putStrLn helpMessage
+          pure 0
+      | otherwise = do
+          putStrLn a
+          pure 1
+
+
+helpMessage :: String
+helpMessage = "usage: ft_turing [-h] jsonfile input\n\npositional arguments:\n  jsonfile\tjson description of the machine\n\n  input\t\tinput of the machine\n\noptional arguments:\n  -h, --help\tshow this help message and exit"
