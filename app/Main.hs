9626e59efa6d9e93af169c613d27f79fcbfd5e61
Marcos <marcos.chiu@gmail.com>
2026-03-21 18:33:32 -0300

    parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,41 @@
 module Main where
 
 import Args (parseArgs)
+import Data.Map qualified as Map
+import Data.Text qualified as T
+import Json (readTuringMachine)
 import Shared (exit)
 import System.Environment (getArgs)
+import TuringMachine
 
 main :: IO ()
 main = do
   args <- getArgs
   case parseArgs args of
-    Left message -> exit message
-    Right (x, y) -> putStrLn ("1st: " ++ x ++ "\n2nd: " ++ y)
+    Prelude.Left message -> exit message
+    Prelude.Right (x, y) -> do
+      result <- readTuringMachine x
+      case result of
+        Prelude.Left err -> putStrLn $ "Error parsing JSON: " ++ err
+        Prelude.Right machine -> do
+          putStrLn $ "Machine name: " ++ T.unpack (name machine)
+          putStrLn $ "Initial state: " ++ T.unpack (initial machine)
+          putStrLn $ "Number of transitions: " ++ show (length (transitions machine))
 
--- https://blog.thomasheartman.com/posts/haskells-maybe-and-either-types
--- https://softwarepatternslexicon.com/haskell/functional-design-patterns/advanced-error-handling-with-either-and-exceptt/
+          case Map.lookup (T.pack "scanright") (transitions machine) of
+            Nothing -> putStrLn "No transitions for scanright"
+            Just transitionsList -> do
+              putStrLn "\nTransitions for scanright:"
+              mapM_ printTransition transitionsList
 
--- type App a = ExceptT ExitCode IO a
--- main = getArgs
---        & liftIO            -- lift IO [String] into App
---        >>= parseArgs
---        >>= openFile
---        >>= parseFile
---        >>= runAnalysis
---        & runExceptT
---        >>= either exitWith (const exitSuccess)
+printTransition :: Transition -> IO ()
+printTransition t =
+  putStrLn $
+    "  Read: "
+      ++ T.unpack (readChar t)
+      ++ " -> "
+      ++ T.unpack (toState t)
+      ++ ", Write: "
+      ++ T.unpack (writeChar t)
+      ++ ", Action: "
+      ++ show (action t)
